f52acfd08ee4f3bcf5fd7d32ce83c3b706418f98
Marvic Sayco <Marvicsayco@eigital.com>
2022-03-19 16:30:46 +0800

    c4 authguard

---
+++ src/app/service/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate{
+  constructor(public router:Router){}
+
+  canActivate(
+    next:ActivatedRouteSnapshot,
+    state:RouterStateSnapshot):Observable<boolean>|Promise<boolean>|boolean{
+      if(localStorage.getItem("email") ===null ){
+      this.router.navigate(['welcome']);
+      }
+      return true;
+    }
+  }
+
